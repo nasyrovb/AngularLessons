9430e193fe9290f54ffc8eec4e1348eeddcbeac9
nasyrovb <bulatik85@gmail.com>
2023-02-21 22:58:17 +0300

    adding header basic layout and navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ItemListComponent } from './components/item-list/item-list.component';
+import { CartComponent } from './components/cart/cart.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ItemListComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {path: '',  component: ItemListComponent},
+      {path: 'cart', component: CartComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
