698dec90cbc58c56ce7e49678f9c43a64d7f71c0
nasyrovb <bulatik85@gmail.com>
2023-03-02 14:30:11 +0300

    modules added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,32 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
-import { ItemListComponent } from './components/item-list/item-list.component';
-import { CartComponent } from './components/cart/cart.component';
-import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
-import { ItemComponent } from './components/item/item.component';
-import { CartItemComponent } from './components/cart-item/cart-item.component';
-import { CartTotalComponent } from './components/cart-total/cart-total.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ItemListComponent,
-    CartComponent,
-    ItemComponent,
-    CartItemComponent,
-    CartTotalComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule,
-    RouterModule.forRoot([
-      {path: '',  component: ItemListComponent},
-      {path: 'cart', component: CartComponent}
-    ])
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
