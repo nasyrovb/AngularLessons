36103b53a1c54a6269036241c1cfeecdd9fce6f0
nasyrovb <bulatik85@gmail.com>
2023-03-20 12:48:08 +0300

    localStorage service added for better testing purposes

--- src/app/state/hydration.effects.ts
+++ src/app/state/hydration.effects.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@angular/core";
 import { Actions, createEffect, ofType, OnInitEffects } from "@ngrx/effects";
 import { Action, Store } from "@ngrx/store";
 import { distinctUntilChanged, map, switchMap, tap } from "rxjs/operators";
+import { BrowserStorageService } from "../storage/storage.service";
 import * as fromRoot from "./app-state";
 import * as HydrationActions from "./hydration.actions";
 
@@ -11,13 +12,13 @@ export class HydrationEffects implements OnInitEffects {
     this.action$.pipe(
       ofType(HydrationActions.hydrate),
       map(() => {
-        const storageValue = localStorage.getItem("state");
+        const storageValue = this.browserStorageService.get("state");
         if (storageValue) {
           try {
             const state = JSON.parse(storageValue);
             return HydrationActions.hydrateSuccess({ state });
           } catch {
-            localStorage.removeItem("state");
+            this.browserStorageService.remove("state");
           }
         }
         return HydrationActions.hydrateFailure();
@@ -31,12 +32,15 @@ export class HydrationEffects implements OnInitEffects {
         ofType(HydrationActions.hydrateSuccess, HydrationActions.hydrateFailure),
         switchMap(() => this.store),
         distinctUntilChanged(),
-        tap((state) => localStorage.setItem("state", JSON.stringify(state)))
+        tap((state) => this.browserStorageService.set("state", JSON.stringify(state)))
       ),
     { dispatch: false }
   );
 
-  constructor(private action$: Actions, private store: Store<fromRoot.AppState>) {}
+  constructor(
+    private action$: Actions, 
+    private store: Store<fromRoot.AppState>, 
+    private browserStorageService: BrowserStorageService) {}
 
   ngrxOnInitEffects(): Action {
     return HydrationActions.hydrate();
